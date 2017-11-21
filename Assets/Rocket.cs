8c60752bc75e6e21e4c97f6d1ac5c8087d664794
Fede <fedemc@gmail.com>
2017-11-20 23:44:42 -0300

    Rocketship, controls, terrain, launchpad.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+
+    }
+}
